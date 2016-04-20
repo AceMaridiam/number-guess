905ded66df35592cbdd3e52ddb351c2b15d99943
AceMaridiam <andermation@gmail.com>
2016-04-20 12:35:24 -0700

    Added reset button

--- number-guess.js
+++ number-guess.js
@@ -1,5 +1,7 @@
 // Button
-var button = document.getElementById('myButton');
+var submit = document.getElementById('myButton');
+// Reset
+var reset = document.getElementById('reset');
 // Create a random number between 0-50
 var num;
 // Get input
@@ -7,32 +9,49 @@ var guess = document.getElementById('guess');
 // Set number of attempts
 var attempts = 5;
 
+// Starts a new game
 function newGame() {
-	// Create a random number between 0-50
 	num = Math.round(Math.random() * 50);
 	guess.placeholder="Make your first guess";
 	document.getElementById("output").innerHTML="You have five attempts to guess my number between 1 and 50";
+	reset.style.display="none";
 }
 
+// Checks if input value is a number
 function numCheck() {
 	if(isNaN(guess.value)) {
 		document.getElementById("output").innerHTML="That's not a number silly.";
-	} if (attempts > 0) {
+	} if(guess.value=="") {
+		document.getElementById("output").innerHTML="Nothing there...";
+	} else {
+		attemptCounter(guess.value);
+		guess.value="";
+	}
+}
+
+// Counts attempts
+function attemptCounter() {
+	if (attempts > 0) {
 		attempts -= 1;
 		document.getElementById("output").innerHTML="Guess number: " + guess.value + 
 		"<br />You have " + attempts + " attempts left.";
 	} if (attempts === 0) {
 		attempts -= 1;
-		document.getElementById("output").innerHTML="You ran out of attempts. <br /><button>Try Again?</button>";
-		
+		document.getElementById("output").innerHTML="You ran out of attempts.";
+		reset.style.display="block";
+	} if ( guess.value === num ) {
+		document.getElementById("output").innerHTML="You got it!";
 	}
-	guess.value="";
 }
 
-button.onclick=function() {
+submit.onclick=function() {
 	console.log("User guess: " + Number(guess.value));
 	console.log("Random Num: " + num);
 	numCheck();
+}
+
+reset.onclick=function(){
+	newGame();
 }
 
 newGame();
